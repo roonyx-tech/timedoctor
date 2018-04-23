31522e740e5c63e10ab463ec7fecabf513bb7548
Eugene Yak <geneayak@gmail.com>
2018-04-23 14:11:54 +0800

    Add extract method

--- lib/timedoctor/client.rb
+++ lib/timedoctor/client.rb
@@ -23,26 +23,32 @@ module Timedoctor
     end
 
     def absent_and_late(params)
-      company_id = params.delete(:company_id)
+      company_id = extract :company_id, params
       exchange :get, "/v1.1/companies/#{company_id}/absent-and-late", params
     end
 
     def edit_absent_and_late(params)
-      company_id = params.delete(:company_id)
+      company_id = extract :company_id, params
       exchange :put, "/v1.1/companies/#{company_id}/absent-and-late", params
     end
 
     def poor_time(params)
-      company_id = params.delete(:company_id)
+      company_id = extract :company_id, params
       exchange :get, "/v1.1/companies/#{company_id}/poortime", params
     end
 
     def web_and_app(params)
-      company_id = params.delete(:company_id)
+      company_id = extract :company_id, params
       exchange :get, "/v1.1/companies/#{company_id}/webandapp", params
     end
 
     private
+
+    def extract(ext, params)
+      extracted = params.delete(ext)
+      raise 'lal' unless extracted
+      extracted
+    end
 
     def exchange(method, url, params = {})
       params[:access_token] = @token
